8a2970dda1b874dc99f0807ab4d6bbb02601cb86
KiranLDA <kiran.dhanjal.adams@gmail.com>
2018-06-20 10:39:00 +0200

    first upload

---
+++ R/PAMLr.R
@@ -0,0 +1,13 @@
+#' PAMLr
+#'
+#' This package manipulates data from SOI-GDL3pam loggers (developped by the Swiss Ornithological Institute). These measure Pressure, Activity, Magnetism and Light.
+#'
+#'
+#' @docType package
+#'
+#' @author Kiran Dhanjal-Adams \email{kiran.dhanjal.adams@gmail.com}
+#'
+#' @name PAMLr
+#'
+#' @import lubridate
+NULL
