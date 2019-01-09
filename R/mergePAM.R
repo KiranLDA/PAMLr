1ee329ce2e9c15bd469199bba471db0ceb130820
KiranLDA <kiran.dhanjal.adams@gmail.com>
2019-01-09 16:15:36 +0100

    mergePAM

--- R/mergePAM.R
+++ R/mergePAM.R
@@ -1,6 +1,6 @@
-#' Integrate classification from classifyPAM into PAM data
+#' merges all the data into one table for interpolation
 #'
-#' @param PAM_data raw pam data see `data(bee_eater` for example
+#' @param dta raw pam data see `data(bee_eater` for example
 #' @param interp logical. whether or not to interpolate if there are NAs
 #' @param keep_NA logical. Whether or not to keep NA
 #'
@@ -15,7 +15,11 @@
 #' @importFrom zoo na.approx
 #'
 #' @export
-mergePAM <- function(PAM_data, interp = TRUE, keep_NA=TRUE ){
+mergePAM <- function(dta,
+                     interp = TRUE,
+                     keep_NA=TRUE){
+
+  PAM_data = dta
 
   if("id" %in% names(PAM_data)){
     to_remove = which(names(PAM_data) == "id")
@@ -27,8 +31,11 @@ mergePAM <- function(PAM_data, interp = TRUE, keep_NA=TRUE ){
   if("obs" %in% colnames(PAM_data$pressure)){
     colnames(PAM_data$pressure)[which(colnames(PAM_data$pressure) == "obs")] = "pressure"
   }
+  if("obs" %in% colnames(PAM_data$temperature)){
+    colnames(PAM_data$temperature)[which(colnames(PAM_data$temperature) == "obs")] = "temperature"
+  }
 
-  new = Reduce(function(...) merge(..., all=keep_NA), PAM_data)
+  new = Reduce(function(...) merge(..., all = keep_NA), PAM_data)
 
   if(interp == TRUE){
     new[,2:ncol(new)] = na.approx(new[,2:ncol(new)])
