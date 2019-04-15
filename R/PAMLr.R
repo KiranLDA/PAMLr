4d8e7d5784c5a99f2efddc08659423fe60a85037
KiranLDA <kiran.dhanjal.adams@gmail.com>
2019-04-15 14:04:36 +0200

    added compareCLASS

--- R/PAMLr.R
+++ R/PAMLr.R
@@ -20,7 +20,7 @@
 #' @importFrom htmltools browsable tagList
 #' @importFrom raster rotate
 #' @importFrom rgl spheres3d abclines3d arrow3d text3d open3d rgl.user2window plot3d rgl.projection
-#' @importFrom stats kmeans aggregate approx gaussian poisson sd complete.cases median cutree as.formula cutree binomial Gamma inverse.gaussian quasipoisson quasibinomial quasi
+#' @importFrom stats kmeans aggregate approx gaussian poisson sd complete.cases median cutree as.formula cutree binomial Gamma inverse.gaussian quasipoisson quasibinomial quasi setNames
 #' @importFrom TwGeos hourOffset as.hour
 #' @importFrom utils read.delim
 #' @importFrom viridis magma viridis
