747080ed07c1559c26a35eae08be21d1a6b38195
KiranLDA <kiran.dhanjal.adams@gmail.com>
2019-05-30 10:15:27 +0200

    added body temperature

--- R/importPAM.R
+++ R/importPAM.R
@@ -3,7 +3,7 @@
 #' @description Imports and formats many datasets into one big nested list containing all the data from the different sensors. A subset of sensors can be selected using `measurements`.
 #'
 #' @param pathname path where files are stored
-#' @param measurements a series of measurements logged by the PAM logger which are to be imported. Currently supports these file extentions: ".pressure", ".glf", ".gle",".acceleration", ".temperature", "AirTemperature" and ".magnetic"
+#' @param measurements a series of measurements logged by the PAM logger which are to be imported. Currently supports these file extentions: ".pressure", ".glf", ".gle",".acceleration", ".temperature", "AirTemperature", ".BodyTemperature" and ".magnetic"
 #'
 #' @return a list of measurements for the one individual
 #'
@@ -25,17 +25,29 @@ importPAM <- function(pathname = pathname,measurements = c(".pressure", ".glf", 
     pressure = read.delim(list.files(pathname,pattern=".pressure",full.names = TRUE),skip=6,sep="",header = FALSE)
     pressure = as.data.frame(list(date=as.POSIXct(strptime(paste(pressure[,1],pressure[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),obs=pressure[,3]))
   }
-  if((".glf" %in% measurements) | (".gle" %in% measurements)){
+  if(".glf" %in% measurements){
     light = read.delim(list.files(pathname,pattern=".glf",full.names = TRUE)[1],skip=6,sep="",header = FALSE)
+    light = as.data.frame(list(date=as.POSIXct(strptime(paste(light[,1],light[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),obs=light[,3]))
+  }
+  if(".gle" %in% measurements){
+    light = read.delim(list.files(pathname,pattern=".gle",full.names = TRUE)[1],skip=6,sep="",header = FALSE)
     light = as.data.frame(list(date=as.POSIXct(strptime(paste(light[,1],light[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),obs=light[,3]))
   }
   if(".acceleration" %in% measurements){
     acceleration = read.delim(list.files(pathname,pattern=".acceleration",full.names = TRUE),skip=6,sep="",header = FALSE)
     acceleration = as.data.frame(list(date=as.POSIXct(strptime(paste(acceleration[,1],acceleration[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),pit=acceleration[,3], act=acceleration[,4]))
   }
-  if((".temperature" %in% measurements)| (".AirTemperature" %in% measurements)){
+  if(".temperature" %in% measurements){
     temperature = read.delim(list.files(pathname,pattern=".temperature",full.names = TRUE),skip=6,sep="",header = FALSE)
     temperature = as.data.frame(list(date=as.POSIXct(strptime(paste(temperature[,1],temperature[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),obs=temperature[,3]))
+  }
+  if(".AirTemperature" %in% measurements){
+    temperature = read.delim(list.files(pathname,pattern=".AirTemperature",full.names = TRUE),skip=6,sep="",header = FALSE)
+    temperature = as.data.frame(list(date=as.POSIXct(strptime(paste(temperature[,1],temperature[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),obs=temperature[,3]))
+  }
+  if (".BodyTemperature" %in% measurements){
+    bodytemperature = read.delim(list.files(pathname,pattern=".BodyTemperature",full.names = TRUE),skip=6,sep="",header = FALSE)
+    bodytemperature = as.data.frame(list(date=as.POSIXct(strptime(paste(temperature[,1],temperature[,2]),tz="UTC",format="%d.%m.%Y %H:%M")),obs=temperature[,3]))
   }
   if(".magnetic" %in% measurements){
     magnetic = read.delim(list.files(pathname,pattern=".magnetic",full.names = TRUE),skip=6,sep="",header = FALSE)
