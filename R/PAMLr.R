d7310b77c00eb0830251ae6ba6f0decd96b08ab0
Kiran DHANJAL-ADAMS <K.Dhanjal-Adams@kew.org>
2025-04-09 17:44:00 +0100

    twilightCalc fixed
    used lubridate in i.preSelection because cut() was removing datetimes with 00:00:00

--- R/PAMLr.R
+++ R/PAMLr.R
@@ -15,9 +15,10 @@
 #' @importFrom dplyr "%>%" distinct last
 #' @importFrom dygraphs dygraph dyRangeSelector dyHighlight dyLegend dyOptions dyShading "%>%"
 #' @importFrom EMbC embc
-#' @importFrom graphics points image mtext axis.POSIXct axis box hist plot abline text par
-#' @importFrom grDevices dev.new rgb xyz.coords adjustcolor
+#' @importFrom graphics points image mtext axis.POSIXct axis box hist plot abline text par identify legend
+#' @importFrom grDevices dev.new rgb xyz.coords adjustcolor graphics.off
 #' @importFrom htmltools browsable tagList
+#' @importFrom lubridate floor_date
 #' @importFrom raster rotate
 #' @importFrom rgl spheres3d abclines3d arrow3d text3d open3d rgl.user2window plot3d rgl.projection
 #' @importFrom stats kmeans aggregate gaussian poisson sd complete.cases median cutree as.formula cutree binomial Gamma inverse.gaussian quasipoisson quasibinomial quasi setNames approx
